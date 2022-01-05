7a248e1f8d536ab53d1013011792cb23e9c00a1f
Aviad Shalom <Aviad.Shalom@Riskified.com>
2022-01-06 00:54:24 +0100

    new location + apis ready to work

--- app/app_runner.py
+++ app/app_runner.py
@@ -11,12 +11,46 @@ from uxui.user_data_convertor_googlesheet import UserDataConvertorGoogleSheetBas
 
 logging.basicConfig(level=logging.INFO)
 
+global LOAD_CONSTRAINTS_FROM_CACHE
 LOAD_CONSTRAINTS_FROM_CACHE = False
-SHOULD_CACHE = False
+SHOULD_CACHE = True
 
-RESULT_SHEET_LOCATION = 'I1:I1'
-EMPLOYEE_SHEET_ID_LOCATION = 'B2:B100'
-RULES_LOCATION = 'D2:F17'
+RESULT_SHEET_LOCATION = 'F5:F5'
+EMPLOYEE_SHEET_ID_LOCATION = 'C9:C104'
+RULES_LOCATION = 'E9:G45'
+SHOULD_USE_CACHE_LOCATION = 'B6:B7'
+
+
+def get_all_rules():
+    rules_table = SpreadsheetClient(MASTER_SHEET).load_cells_given_pair(RULES_LOCATION)
+    disabled_rules = [int(rule[0].text) for rule in rules_table if len(rule) == 3 and rule[2].text == 'False']
+    return [rule for rule in Rules.get_all_rules() if rule.get_id() not in disabled_rules]
+
+
+def cache_employee_mid_week_constraints(constraints_to_cache):
+    with open('constraints_midweek_file', 'wb') as constraints_midweek_file:
+        logging.info("caching config is: {}".format(SHOULD_CACHE))
+        if SHOULD_CACHE:
+            pickle.dump(constraints_to_cache, constraints_midweek_file)
+
+
+def load_constraints_mid_week_from_cache():
+    with open('constraints_midweek_file', 'rb') as constraints_midweek_file:
+        midweek_constrains = pickle.load(constraints_midweek_file)
+        return midweek_constrains
+
+
+def cache_employee_weekend_constraints(constraints_to_cache, employees_to_cache):
+    with open('constraints_employees_file', 'wb') as constraints_employees_file:
+        logging.info("caching config is: {}".format(SHOULD_CACHE))
+        if SHOULD_CACHE:
+            pickle.dump((constraints_to_cache, employees_to_cache), constraints_employees_file)
+
+
+def load_constraints_weekend_from_cache():
+    with open('constraints_employees_file', 'rb') as constraints_employees_file:
+        constraints_cached, employees_cached = pickle.load(constraints_employees_file)
+        return constraints_cached, employees_cached
 
 
 class AppRunner:
@@ -24,6 +58,18 @@ class AppRunner:
     def __init__(self):
         self.board = PlanningBoard()
         self.kill_process = False
+        self.loading_bar = 0
+        self.message = ''
+        self.employees = []
+        self.did_weekend_finished = False
+        self.did_midweek_started = False
+        self.use_cached_constraints = False
+
+    @staticmethod
+    def should_load_constraint_from_cached():
+        master_sheet_client = SpreadsheetClient(MASTER_SHEET)
+        global LOAD_CONSTRAINTS_FROM_CACHE
+        LOAD_CONSTRAINTS_FROM_CACHE = master_sheet_client.load_cells_given_pair(SHOULD_USE_CACHE_LOCATION)[0][0].text == 'True'
 
     @staticmethod
     def get_all_employee_ids():
@@ -37,10 +83,16 @@ class AppRunner:
         result_table = master_sheet_client.load_cells_given_pair(RESULT_SHEET_LOCATION)
         return result_table[0][0].text
 
+    def update_status_message(self, num_str, flow):
+        self.message = 'flow: {}, percentage: {}'.format(flow, num_str)
+
     def get_employees_and_their_constraints_for_weekend(self):
+        self.loading_bar = 0
         constraints = []
         employees = []
-        for sheet_id in self.get_all_employee_ids():
+        ids_from_sheet = self.get_all_employee_ids()
+        base = len(ids_from_sheet)
+        for sheet_id in ids_from_sheet:
             self.kill_process_if_needed()
             data_convertor = UserDataConvertorGoogleSheetBased(SpreadsheetClient(sheet_id))
             employee = data_convertor.get_employee_details()
@@ -54,12 +106,18 @@ class AppRunner:
                 data_convertor.get_constraint_for_weekend(weekend, weekend_constraints)
             constraints.append(weekend_constraints)
             time.sleep(10)
+            self.loading_bar = self.loading_bar + 1
+            self.update_status_message(str((self.loading_bar/base)*100) + '%', 'weekend')
+        self.employees = employees
         return constraints, employees
 
     def get_employees_and_their_constraints_for_midweek(self):
+        self.loading_bar = 0
         constraints = []
         employees = []
-        for sheet_id in self.get_all_employee_ids():
+        ids_from_sheet = self.get_all_employee_ids()
+        base = len(ids_from_sheet)
+        for sheet_id in ids_from_sheet:
             self.kill_process_if_needed()
             data_convertor = UserDataConvertorGoogleSheetBased(SpreadsheetClient(sheet_id))
             employee = data_convertor.get_employee_details()
@@ -73,25 +131,26 @@ class AppRunner:
                 data_convertor.get_constraint_for_midweek(week, midweek_constraints)
             constraints.append(midweek_constraints)
             time.sleep(10)
+            self.loading_bar = +1
+            self.update_status_message(str((self.loading_bar/base)*100) + '%', 'mid-week')
         return constraints, employees
-
-    def get_all_rules(self):
-        rules_info = SpreadsheetClient(MASTER_SHEET).load_cells_given_pair(RULES_LOCATION)
-        disabled_rules = [int(rule[0].text) for rule in rules_info if rule[2].text == 'False']
-        return [rule for rule in Rules.get_all_rules() if rule.get_id() not in disabled_rules]
-
-
 
     def get_result_sheet_convertor(self):
         result_sheet_id = self.get_result_sheet()
         convertor = UserDataConvertorGoogleSheetBased(SpreadsheetClient(result_sheet_id))
         return convertor
 
-    def run_algorithm_for_weekend(self, employees, constraints):
+    def run_algorithm_for_weekend(self, constraints):
+        self.loading_bar = 0
+        employees = self.employees
         weight = 99999
         chosen_board = self.board
         logging.info("weekend: going over on 6000 randomized options")
-        for i in range(6000):
+        base = 6000
+        chunk = base / len(employees)
+        rules = get_all_rules()
+
+        for i in range(base):
             self.kill_process_if_needed()
             if weight == 0:
                 break
@@ -101,27 +160,36 @@ class AppRunner:
             employee_fresh = copy.deepcopy(employees)
 
             algorithm_based_priority_queue.solve_weekend(board_for_test, employee_fresh, constraints, False,
-                                                         self.get_all_rules())
+                                                         rules)
             new_weight = board_weighter.weight_weekend(board_for_test)
             if new_weight < weight:
                 weight = new_weight
                 chosen_board = board_for_test
             else:
-                del board_for_test
+                del board_for_test, employee_fresh
                 gc.collect()
+            self.loading_bar = self.loading_bar + chunk
+            self.update_status_message(str((self.loading_bar/base)*100) + '%', 'weekend-algo')
 
         self.board = chosen_board
 
         convertor = self.get_result_sheet_convertor()
         for weekend in WeekOfTheMonth:
+            self.kill_process_if_needed()
             time.sleep(6)
             convertor.write_weekend(weekend, self.board)
+        self.update_status_message('DONE', 'weekend-algo')
 
-    def run_algorithm_for_midweek(self, employees, constraints):
+    def run_algorithm_for_midweek(self, constraints):
+        self.loading_bar = 0
+        employees = self.employees
         weight = 99999
         chosen_board = self.board
         logging.info("midweek: going over on 4000 randomized options")
-        for i in range(4000):
+        base = 4000
+        chunk = base / len(employees)
+        rules = get_all_rules()
+        for i in range(base):
             self.kill_process_if_needed()
             if weight == 0:
                 break
@@ -131,71 +199,64 @@ class AppRunner:
             random.shuffle(constraints)
             employee_fresh = copy.deepcopy(employees)
             algorithm_based_priority_queue.solve_mid_week(board_for_test, employee_fresh, constraints, False,
-                                                          self.get_all_rules())
+                                                          rules)
 
             new_weight = board_weighter.weight_midweek(board_for_test)
             if new_weight < weight:
                 weight = new_weight
                 chosen_board = board_for_test
             else:
-                del board_for_test
+                del board_for_test, employee_fresh
                 gc.collect()
+            self.loading_bar = self.loading_bar + chunk
+            self.update_status_message(str((self.loading_bar/base)*100) + '%', 'mid-week-algo')
 
         self.board = chosen_board
         convertor = self.get_result_sheet_convertor()
         for week in WeekOfTheMonth:
+            self.kill_process_if_needed()
             time.sleep(6)
             convertor.write_mid_week(week, self.board)
+        self.update_status_message('DONE', 'midweek-algo')
 
-    def load_constraints_weekend_from_cache(self):
-        with open('constraints_employees_file', 'rb') as constraints_employees_file:
-            constraints_cached, employees_cached = pickle.load(constraints_employees_file)
-            return constraints_cached, employees_cached
-
-    def cache_employee_weekend_constraints(self, constraints_to_cache, employees_to_cache):
-        with open('constraints_employees_file', 'wb') as constraints_employees_file:
-            logging.info("caching config is: {}".format(SHOULD_CACHE))
-            if SHOULD_CACHE:
-                pickle.dump((constraints_to_cache, employees_to_cache), constraints_employees_file)
-
-    def load_constraints_mid_week_from_cache(self):
-        with open('constraints_midweek_file', 'rb') as constraints_midweek_file:
-            midweek_constrains = pickle.load(constraints_midweek_file)
-            return midweek_constrains
-
-    def cache_employee_mid_week_constraints(self, constraints_to_cache):
-        with open('constraints_midweek_file', 'wb') as constraints_midweek_file:
-            logging.info("caching config is: {}".format(SHOULD_CACHE))
-            if SHOULD_CACHE:
-                pickle.dump(constraints_to_cache, constraints_midweek_file)
 
     @staticmethod
-    def trigger_flow(master_sheet_id, app_runner):
+    def mid_week_flow(app_runner, app_runners, master_sheet_id):
+        app_runner.did_midweek_started = True
         global MASTER_SHEET
-
         MASTER_SHEET = master_sheet_id
-        app_runner = app_runner
 
         if LOAD_CONSTRAINTS_FROM_CACHE:
-            logging.info("loading constraints from cache")
-            weekend_constraints, employees = app_runner.load_constraints_weekend_from_cache()
-        else:
-            logging.info("loading weekend constraints from google sheets")
-            weekend_constraints, employees = app_runner.get_employees_and_their_constraints_for_weekend()
-            app_runner.cache_employee_weekend_constraints(weekend_constraints, employees)
-        app_runner.run_algorithm_for_weekend(employees, weekend_constraints)
-        del weekend_constraints
-        gc.collect()
-        if LOAD_CONSTRAINTS_FROM_CACHE:
-            mid_week_constraints = app_runner.load_constraints_mid_week_from_cache()
+            mid_week_constraints = load_constraints_mid_week_from_cache()
         else:
             logging.info("loading midweek constraints from google sheets")
             mid_week_constraints, employees = app_runner.get_employees_and_their_constraints_for_midweek()
             app_runner.cache_employee_mid_week_constraints(mid_week_constraints)
-        app_runner.run_algorithm_for_midweek(employees, mid_week_constraints)
-        del mid_week_constraints, employees
+        app_runner.run_algorithm_for_midweek(mid_week_constraints)
+        del mid_week_constraints
         gc.collect()
+        app_runners.clear()
+        app_runner.did_weekend_finished = False
+        app_runner.did_midweek_started = False
 
+    @staticmethod
+    def weekend_flow(app_runner, master_sheet_id):
+        global MASTER_SHEET
+        MASTER_SHEET = master_sheet_id
+        app_runner.should_load_constraint_from_cached()
+        if LOAD_CONSTRAINTS_FROM_CACHE:
+            logging.info("loading constraints from cache")
+            weekend_constraints, employees = load_constraints_weekend_from_cache()
+            app_runner.employees = employees
+        else:
+            logging.info("loading weekend constraints from google sheets")
+            weekend_constraints, employees = app_runner.get_employees_and_their_constraints_for_weekend()
+            cache_employee_weekend_constraints(weekend_constraints, employees)
+        app_runner.run_algorithm_for_weekend(weekend_constraints)
+        del weekend_constraints
+        gc.collect()
+        app_runner.did_weekend_finished = True
+        return employees
 
     def kill_process_if_needed(self):
         if self.kill_process:
--- main.py
+++ main.py
@@ -15,24 +15,58 @@ MASTER_SHEET_ID = sys.argv[1:][0]
 app = Flask(__name__)
 list_of_app_runner = []
 
-@app.route('/trigger_sched_solver/', methods=['POST'])
-def trigger_algorithm():
+
+@app.route('/sched_solver_status/', methods=['POST'])
+def sched_solver_status():
+    if 'almog_tania_aviad_3981' != request.form['key']:
+            return 'not process has started'
+    else:
+        if len(list_of_app_runner) == 0:
+            return 'nothing is happening!'
+        if len(list_of_app_runner) == 1:
+            return list_of_app_runner[0].message
+
+
+@app.route('/trigger_sched_solver_weekend/', methods=['POST'])
+def trigger_sched_solver_weekend():
     if 'almog_tania_aviad_3981' != request.form['key']:
         return 'key_was_not_provided'
     else:
         if len(list_of_app_runner) == 0:
             logging.info("preparation for running algo")
             app_runner = AppRunner()
-            thread = Thread(target=AppRunner.trigger_flow, args=(MASTER_SHEET_ID, app_runner, ))
+            thread = Thread(target=AppRunner.weekend_flow, args=(app_runner, MASTER_SHEET_ID))
             thread.start()
             list_of_app_runner.append(app_runner)
             return 'algorithm_triggered'
         else:
-            return 'there are still active session'
+            return 'there are still active session stop weekend to delete and start again'
 
 
-@app.route('/stop_sched_solver/', methods=['POST'])
-def stop_algorithm():
+@app.route('/trigger_sched_solver_midweek/', methods=['POST'])
+def trigger_sched_solver_midweek():
+    if 'almog_tania_aviad_3981' != request.form['key']:
+        return 'key_was_not_provided'
+
+    if len(list_of_app_runner) == 0:
+        return 'please start with weekend flow'
+
+    if len(list_of_app_runner) == 1 and not list_of_app_runner[0].did_weekend_finished:
+        return 'weekend wasn\'t finished'
+
+    else:
+        if len(list_of_app_runner) == 1 and not list_of_app_runner[0].did_midweek_started:
+            logging.info("preparation for running algo")
+            #MAKE SURE GIVE LIST OF APP_RUNNER IN ORDER TO STOP
+            thread = Thread(target=AppRunner.mid_week_flow, args=(list_of_app_runner[0], list_of_app_runner, MASTER_SHEET_ID))
+            thread.start()
+            return 'algorithm_triggered'
+        else:
+            return 'there are still active session Or u didn\'t active weekend'
+
+
+@app.route('/stop_sched_solver_weekend/', methods=['POST'])
+def stop_sched_solver_weekend():
     if 'almog_tania_aviad_3981' != request.form['key']:
         return 'key_was_not_provided'
     else:
@@ -42,11 +76,18 @@ def stop_algorithm():
         return 'stopped algorithm'
 
 
+@app.route('/stop_sched_solver_mid_week/', methods=['POST'])
+def stop_sched_solver_mid_week():
+    if 'almog_tania_aviad_3981' != request.form['key']:
+        return 'key_was_not_provided'
+    else:
+        for app_runner in list_of_app_runner:
+            app_runner.kill_process = True
+        return 'stopped algorithm'
+
+
 if __name__ == '__main__':
     server = gevent.pywsgi.WSGIServer( (u'0.0.0.0', 5042), app, handler_class=WebSocketHandler)
     logging.info("server is up and running")
     server.serve_forever()
 
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5042, debug=False)
